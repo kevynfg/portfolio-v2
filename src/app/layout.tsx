7d671eb49cca878cf4a8887fc99934ab2ebcec44
Kevyn Gonçalves <kevynredlotus@gmail.com>
2023-02-24 12:32:58 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,26 @@
+import AnalyticsWrapper from "@/components/analytics"
+import { Metadata } from "next";
+import clsx from 'clsx';
 import './globals.css'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Kevyn Gonçalves',
+    template: '%s | Kevyn Gonçalves',
+  },
+  description: 'Developer, writer, and creator.',
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  }
+};
 
 export default function RootLayout({
   children,
@@ -6,13 +28,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={clsx("text-black bg-gray-900 dark:text-white")}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="antialiased">
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   )
 }
